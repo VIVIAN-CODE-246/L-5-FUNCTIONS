a65daa28794c03d5f6a5c65bb5a9818a6d56384d
VIVIAN-CODE-246 <zaraconcept247@gmail.com>
2025-01-18 13:53:30 +0200

    FUNCTIONS IN PYTHON

---
+++ main.py
@@ -0,0 +1,20 @@
+def introduce (name, age):
+    print("My name is", name)
+    print("I am", age, "years old.")
+
+introduce ("Vivian", 14)
+introduce ("Grace", 10)
+
+def add(x,y):
+    return x + y
+def subtract(a,b):
+    return a - b
+def divide(d,e):
+    return d / e
+def multiply(x,y):
+    return x * y
+def squareroot(a):
+    return a ** 0.5
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+print(squareroot(num1))
